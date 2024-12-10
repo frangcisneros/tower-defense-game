ce5971dec1cb88743a986ac15754adee82e18e18
Francisco Cisneros <franbichir@gmail.com>
2024-12-10 15:59:52 -0300

    configuracion de botones para que obtengan las torres a las que corresponden, igual no funciona como quiero

---
+++ ui/buttons/tower_button.gd
@@ -0,0 +1,9 @@
+extends Button
+
+@onready var avaiableButtons = get_parent().get_parent().avaiableButtons
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
--- ui/lvl_ui.gd
+++ ui/lvl_ui.gd
@@ -4,15 +4,20 @@ extends Control
 @onready var towerButton = preload("res://ui/buttons/tower-button.tscn")
 @onready var hTowerContainer = $"h-tower-container"
 var avaiableTowers : Array
+@export var avaiableButtons: Dictionary = {}
+var count = 0
 
 func _ready() -> void:
 	avaiableTowers = get_towers(lvlScene)
+	# funcion para obtener un diccionario con los nombres de los botones, capaz no me hace falta la funcion get_towers
+	# voy a configurar para que cada boton obtenga a que torre corresponda pero dentro del objeto boton
+	# no pude hacer una funcion no se muy bien por que, por ahora lo dejo asi
 	for tower in avaiableTowers:
 		var tempTower = towerButton.instantiate()
 		hTowerContainer.add_child(tempTower)
+		avaiableButtons[hTowerContainer.get_child(count)] = avaiableTowers[count]
+		count += 1
 	
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
